4dc44459474a92d8bc23a0d565e7fe19c72e7a62
Avishkar-QE <avishkarsorate95@gmail.com>
2024-09-20 07:15:41 +0530

    ternary operator

--- src/sept/ex18092024/Type_casting.java
+++ src/sept/ex18092024/Type_casting.java
@@ -18,7 +18,7 @@ public class Type_casting {
         // implicit casting not possible as we get syntax error
 
         //explicit casting narrowing cause loss of data
-        int g = 990;
+        int g = 990; 
         byte p = (byte)g;
         System.out.println(p);
     }
---
+++ src/sept/ex20092024/Ternary_operator.java
@@ -0,0 +1,10 @@
+package sept.ex20092024;
+
+public class Ternary_operator {
+    public static void main(String[] args) {
+        //condition ? expression_if_true: expression_if_false
+
+        int a = (30>40)? 80:90;
+    System.out.println(a);
+    }
+}
