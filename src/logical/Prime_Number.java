7a4993b59ffab184af51f823d02309feb92482ef
Avishkar-QE <avishkarsorate95@gmail.com>
2024-10-05 10:40:55 +0530

    prime number

---
+++ src/logical/Prime_Number.java
@@ -0,0 +1,25 @@
+package logical;
+
+public class Prime_Number {
+    public static void main(String[] args)
+    {
+        int n = 117;
+        int count = 0;
+
+        for (int i=1;i<=n;i++)
+        {
+            if (n%i==0)
+            {
+                count++;
+            }
+        }
+        if (count==2)
+        {
+            System.out.println("Prime Number");
+        }
+        else
+        {
+            System.out.println("Not a Prime Number");
+        }
+    }
+}
