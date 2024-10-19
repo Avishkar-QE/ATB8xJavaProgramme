3c32cccce64ad897238de9b0113dc8e8d7a19f7d
Avishkar-QE <avishkarsorate95@gmail.com>
2024-10-19 21:22:13 +0530

    encapsulation

---
+++ src/october/encapsulation/Implimentation.java
@@ -0,0 +1,12 @@
+package october.encapsulation;
+
+public class Implimentation {
+
+    public static void main(String[] args) {
+
+        Hider H = new Hider("Avishkar","Floki");
+        System.out.println(H.getUserName());
+        System.out.println(H.getPassword());
+    }
+    
+}
