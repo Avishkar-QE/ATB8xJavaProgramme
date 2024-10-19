4e94ffcf76608b97520e6d2793e5201aa5f31567
Avishkar-QE <avishkarsorate95@gmail.com>
2024-10-19 21:41:34 +0530

    comments

--- src/october/encapsulation/example/ICICIBank.java
+++ src/october/encapsulation/example/ICICIBank.java
@@ -1,4 +1,33 @@
 package october.encapsulation.example;
 
 public class ICICIBank {
+    private String name;
+    private int balance;
+
+    public ICICIBank(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance,boolean is_Admin) {
+        if (is_Admin) {
+            this.balance = balance;
+        }
+        else
+        {
+            System.out.println("Not Allowed");
+        }
+    }
 }
--- src/october/encapsulation/example/Userface.java
+++ src/october/encapsulation/example/Userface.java
@@ -1,4 +1,14 @@
 package october.encapsulation.example;
 
 public class Userface {
+    public static void main(String[] args) {
+
+        ICICIBank avishkar = new ICICIBank("Avishkar",100000000);
+        System.out.println(avishkar.getBalance());
+
+        boolean Admin = true; // We can add code to check if authenticated here to allow setter method
+        avishkar.setBalance(500000000,Admin);
+        System.out.println(avishkar.getBalance());
+
+    }
 }
